17125e392c8f62d6417ab3a88393d2b8fcc97662
luanportesalves <lportes550@gmail.com>
2024-12-16 03:41:10 -0300

    Update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 function toggle(el) {
     var display = document.getElementById(el).style.display;
+    document.getElementById(el).style.display = 'none';
     if(display == "none") {
         document.getElementById(el).style.display = 'block';
     }
