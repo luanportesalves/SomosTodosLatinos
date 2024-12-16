7e8159e17f2d32dfc7103dec3f10b745ce5e54fe
luanportesalves <lportes550@gmail.com>
2024-12-16 04:39:48 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 function toggle(conteudo) {
     var display = document.getElementById("conteudo").style.display;
+    document.getElementById("conteudo").style.display = 'none';
+    document.getElementsByClassName("qrcode").style.justifyContent = 'center';
     if(display == "none") {
         document.getElementById("conteudo").style.display = 'block';
         document.getElementsByClassName("qrcode").style.justifyContent = 'center';
