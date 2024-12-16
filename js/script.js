98ca6fb02c12d379ec120e7ca42f87668d8eab3e
luanportesalves <lportes550@gmail.com>
2024-12-16 04:55:51 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 function toggle(el) {
+    var display = getElementsByClassName(el).style.display;
     if(display == "none") {
         document.getElementsByClassName(el).style.display = 'block';
         document.getElementsByClassName(el).style.justifyContent = 'center';
