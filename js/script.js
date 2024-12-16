684ac55330e340d5624d92da551abdd513b91403
luanportesalves <lportes550@gmail.com>
2024-12-16 04:49:58 -0300

    Att

--- js/script.js
+++ js/script.js
@@ -1,14 +1,12 @@
-function toggle(conteudo) {
-    var display = document.getElementById("conteudo").style.display;
-    document.getElementById("conteudo").style.display = 'none';
+function toggle(qrcode) {
     document.getElementsByClassName("qrcode").style.justifyContent = 'center';
     if(display == "none") {
-        document.getElementById("conteudo").style.display = 'block';
+        document.getElementsByClassName("qrcode").style.display = 'block';
         document.getElementsByClassName("qrcode").style.justifyContent = 'center';
     }
     else {
 
-        document.getElementById("conteudo").style.display = 'none';
+        document.getElementsByClassName("qrcode").style.display = 'none';
         document.getElementsByClassName("qrcode").style.justifyContent = 'center';
     }
 }
