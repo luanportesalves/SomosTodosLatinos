1cee38b67720c71307abd150620e58ce2524969d
luanportesalves <lportes550@gmail.com>
2024-12-16 04:57:42 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,11 +2,11 @@ function toggle(el) {
     var display = getElementsByClassName(el).style.display;
     if(display == "none") {
         document.getElementsByClassName(el).style.display = 'block';
-        document.getElementsByClassName(el).style.justifyContent = 'center';
+
     }
     else {
 
         document.getElementsByClassName(el).style.display = 'none';
-        document.getElementsByClassName(el).style.justifyContent = 'center';
+
     }
 }
